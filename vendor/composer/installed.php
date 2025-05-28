4b3b7015ba0f2a6a086db57ae3767ac2b84e859c
Philipp Hofmann <philipp@hofmann-ebs.de>
2025-05-28 17:32:42 +0200

    Clean up: Build files are not included in the commits anymore.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'pytonballoon810/kicad_viewer',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '4579065109e29243e12507fcabdec99d03ee109d',
+        'reference' => '89ffbda3f2b5c82a629b7c08a4aec1909e8fede6',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'pytonballoon810/kicad_viewer' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '4579065109e29243e12507fcabdec99d03ee109d',
+            'reference' => '89ffbda3f2b5c82a629b7c08a4aec1909e8fede6',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
