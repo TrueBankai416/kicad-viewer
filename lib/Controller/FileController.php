e0ca2c5a73bd017145b5ce2fc728434bac20bcf9
MentatBot <160964065+MentatBot@users.noreply.github.com>
2025-05-29 00:11:06 +0000

    Simplify controller for debugging route registration
    **Debugging approach - simplified controller to test basic routing:**
    
    1. **Removed manual controller registration** - modern Nextcloud should auto-discover controllers
    2. **Simplified FileController** to return basic JSON response instead of file content
    3. **Removed complex dependencies** (IRootFolder, IUserSession) that might cause issues
    
    **Test steps:**
    1. Disable/enable app: `php occ app:disable kicad_viewer && php occ app:enable kicad_viewer`
    2. Check routes: `php occ route:list | grep kicad_viewer`
    3. Test endpoint: Should return JSON instead of white page
    
    **Expected result:**
    ```json
    {
      "message": "FileController is working!",
      "path": "Arduino_Uno_R3_From_Scratch.kicad_sch",
      "filename": "Arduino_Uno_R3_From_Scratch.kicad_sch"
    }
    ```
    
    If this works, we know routing is functional and can add back file operations. If not, there's a deeper routing issue to solve.

--- lib/Controller/FileController.php
+++ lib/Controller/FileController.php
@@ -8,25 +8,12 @@ namespace OCA\kicad_viewer\Controller;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
-use OCP\AppFramework\Http\StreamResponse;
-use OCP\Files\IRootFolder;
 use OCP\IRequest;
-use OCP\IUserSession;
 
 class FileController extends Controller {
     
-    private IRootFolder $rootFolder;
-    private IUserSession $userSession;
-    
-    public function __construct(
-        string $appName,
-        IRequest $request,
-        IRootFolder $rootFolder,
-        IUserSession $userSession
-    ) {
+    public function __construct(string $appName, IRequest $request) {
         parent::__construct($appName, $request);
-        $this->rootFolder = $rootFolder;
-        $this->userSession = $userSession;
     }
     
     /**
@@ -34,38 +21,12 @@ class FileController extends Controller {
      * @NoCSRFRequired
      * @PublicPage
      */
-    public function getFile(string $path, string $filename): StreamResponse {
-        try {
-            $user = $this->userSession->getUser();
-            if (!$user) {
-                return new StreamResponse('', Http::STATUS_UNAUTHORIZED);
-            }
-            
-            $userFolder = $this->rootFolder->getUserFolder($user->getUID());
-            
-            // If path is just the filename, use it directly
-            // Otherwise treat path as the full file path
-            $filePath = ($path === $filename) ? $filename : $path;
-            $file = $userFolder->get($filePath);
-            
-            if (!$file || $file->getType() !== \OCP\Files\FileInfo::TYPE_FILE) {
-                return new StreamResponse('', Http::STATUS_NOT_FOUND);
-            }
-            
-            // Get the file content
-            $content = $file->getContent();
-            $mimeType = $file->getMimeType();
-            
-            // Create a stream response with proper headers
-            $response = new StreamResponse($content);
-            $response->addHeader('Content-Type', $mimeType);
-            $response->addHeader('Content-Disposition', 'inline; filename="' . $filename . '"');
-            $response->addHeader('Content-Length', (string)strlen($content));
-            
-            return $response;
-            
-        } catch (\Exception $e) {
-            return new StreamResponse('', Http::STATUS_INTERNAL_SERVER_ERROR);
-        }
+    public function getFile(string $path, string $filename): DataResponse {
+        // Simple test response first
+        return new DataResponse([
+            'message' => 'FileController is working!',
+            'path' => $path,
+            'filename' => $filename
+        ]);
     }
 }
